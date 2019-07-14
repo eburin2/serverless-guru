e4f88a40435b61c674dca0cc6415f029ada8e1cf
Elliot Burin <eburin2@gmail.com>
2019-07-14 13:38:45 -0700

    add index and manifest back to public

---
+++ build/precache-manifest.cd94251fda4bbaedc5a6178861d62e94.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee9cb98a3792021767f7c55ab89d2c85",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9123a13b6f800fc66523",
+    "url": "/static/css/main.2c9ecb2f.chunk.css"
+  },
+  {
+    "revision": "b53703b08aae6ecc477b",
+    "url": "/static/js/2.a00048a7.chunk.js"
+  },
+  {
+    "revision": "9123a13b6f800fc66523",
+    "url": "/static/js/main.d82831b7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
